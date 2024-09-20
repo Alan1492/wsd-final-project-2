e8ff38edf4b6e33f1bf61b362d580c6725fe0dec
Alan1492 <alanabelabraham75@gmail.com>
2024-09-20 21:46:41 +0530

    files upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./components/Home Page/Home";
+import Menu from "./components/Menu/Menu"
+ import  {Login } from "./components/User/login";
+ import { Register } from "./components/User/register";
+ import './App.css';
+import Checkout from "./components/cart/Checkout";
+import Payment from "./components/Payment/Payment";
+import Contactus from "./components/Contactus/Contactus";
+import Aboutus from "./components/Aboutus/Aboutus";
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="background">
+    <BrowserRouter>          
+          <div className="container"></div>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/Home" element={<Menu/>} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<Checkout/>} />
+              <Route path="/payment" element={<Payment/>}/>
+              <Route path="/aboutus" element={<Aboutus/>} />
+              <Route path="/contactus" element={<Contactus/>} />
+              
+            
+            </Routes>
+          
+        </BrowserRouter>
+        
+        </div>
+
   );
 }
 
